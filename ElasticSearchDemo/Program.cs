fd2039dd5529bf45474315b4f24e7aeb1d17c7a0
JakeLardinois <jake.lardinois@gmail.com>
2017-03-10 10:51:40 -0600

    Added new Elastic Search Demo Project
    the new version of NEST does not work with my installed version of
    Elastic Search, so I add this project to interface with my installed
    version

--- ElasticSearchDemo/Program.cs
+++ ElasticSearchDemo/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nest;
 
 namespace ElasticSearchDemo
 {
     class Program
     {
+        public static Uri node;
+        public static ConnectionSettings settings;
+        public static ElasticClient client;
+
         static void Main(string[] args)
         {
+            node = new Uri("http://localhost:9200");
+            settings = new ConnectionSettings(node);
+            settings.DefaultIndex("my_blog");
+            client = new ElasticClient(settings);
+
+
+            var CreateIndexResponse = client.CreateIndex("my_blog", c => c
+                .Mappings(ms => 
+                    ms.Map<Post>(m => m.AutoMap()))
+                .Settings(s => s
+                    .NumberOfShards(1)
+                    .NumberOfReplicas(1))
+            );
+
+            var foo = "bar";
         }
     }
 }
